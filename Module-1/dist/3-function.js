2ad05ced9d3015b2b7fcab4e17400ae3104d9c11
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-23 19:19:55 +0600

    Learn : functions normal function, arrow function, callback function

---
+++ Module-1/dist/3-function.js
@@ -0,0 +1,17 @@
+"use strict";
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// 1. Normal function in TS
+function add(num1, num2) {
+    return num1 + num2;
+}
+const sum = add(10, 40);
+// console.log(sum);
+// 2.  Arrow function in TS
+// const AddTwoNumber = (num1:number, num2:number):string => {
+//     return `${num1} + ${num2} =  ${num1 + num2} `;
+// }
+// const sum1 = AddTwoNumber(30, 40); // 30 + 40 =  70 
+// const nums:number[] = [2,4,7];
+// const newArr = nums.m
