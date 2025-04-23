b45ee6ceecfc16f11f735dd130f38dfba0b7e5da
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-23 15:09:36 +0600

    Learn : Object, optional propertity of object and literal type

---
+++ Module-1/dist/2-object-optional-literal-type.js
@@ -0,0 +1,10 @@
+"use strict";
+// Reference Type --> Object
+const user = {
+    firstName: "Shakil",
+    middleName: "",
+    lastName: "Ahmed",
+    isMarried: false,
+    company: "Programming Hero",
+};
+//   user.company = "ph"; // Cannot assign to 'company' because it is a read-only property.
