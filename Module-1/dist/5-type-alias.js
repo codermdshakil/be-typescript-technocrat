dd306b12e27fc4b54edbd74b7368087c88c50523
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-23 20:22:06 +0600

    * Type Alas  *  * - Object type alias  * - Array type alias  * - Variable type alias  * - function type alias  *

---
+++ Module-1/dist/5-type-alias.js
@@ -0,0 +1,27 @@
+"use strict";
+{
+    const student1 = {
+        name: 'Shakil Ahmed',
+        age: 20,
+        email: 'ahmedshakil@gmail.com',
+        phoneNo: '0170000000',
+        isMarried: false,
+        address: 'Dhaka'
+    };
+    const student2 = {
+        name: 'Noyon Raham',
+        age: 22,
+        email: 'noyonrahman2003@gmail.com',
+        address: 'Dhala'
+    };
+    const persons = [
+        { name: 'Shakil AHmed', age: 20 },
+        { name: 'Noyon Rahman', age: 22 },
+        { name: 'Nadim Hassan', age: 21 }
+    ];
+    const username = 'Shakil Ahmed';
+    const isAdmin = false;
+    const add = (n1, n2) => n1 + n2;
+    // console.log(add(10, 20));
+    // console.log(add(10, 20));
+}
---
+++ Module-1/src/5-type-alias.ts
@@ -0,0 +1,80 @@
+{
+ /**
+ * 
+ * Type Alas
+ * 
+ * - Object type alias
+ * - Array type alias
+ * - Variable type alias
+ * - function type alias
+ * 
+ * */ 
+
+    // object type alias
+
+    type Student = { 
+
+        name:string,
+        age:number,
+        email:string,
+        phoneNo?:string,
+        isMarried?:boolean,
+        address?:string,
+    };
+
+
+    const student1 : Student  ={
+        name:'Shakil Ahmed',
+        age:20,
+        email:'ahmedshakil@gmail.com',
+        phoneNo:'0170000000',
+        isMarried:false,
+        address:'Dhaka'
+    };
+
+
+    const student2 :Student  = {
+        name:'Noyon Raham',
+        age:22,
+        email:'noyonrahman2003@gmail.com',
+        address:'Dhala'
+    }
+
+    // array type alias
+
+    type Person = {
+        name:string,
+        age:number
+    };
+
+    const persons :Person[] = [
+        {name:'Shakil AHmed', age:20},
+        {name:'Noyon Rahman', age:22},
+        {name:'Nadim Hassan', age:21}
+    ];
+
+
+
+    // variable type alias
+
+    type UserName = string;
+    type IsAdmin = boolean;
+    
+    const username:UserName = 'Shakil Ahmed';
+    const isAdmin : IsAdmin =false;
+ 
+
+    // function type alias
+    type Add = (n1:number, n2:number) => number;
+
+
+    const add:Add = (n1, n2) => n1 + n2;
+    
+    // console.log(add(10, 20));
+    // console.log(add(10, 20));
+
+
+
+}
+
+
