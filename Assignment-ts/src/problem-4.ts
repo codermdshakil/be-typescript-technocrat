76830b8c7d205c8bcf6f5693bc3f8c3a974aa62d
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-29 11:29:09 +0600

    Problem Solved - 4: reverseArray

---
+++ Assignment-ts/src/problem-4.ts
@@ -0,0 +1,10 @@
+
+const reverseArray = (arr:any[]) => {
+   
+    arr.reverse()
+    console.log(arr);
+
+};
+
+// reverseArray(["apple", "banana", "cherry"]); // [ 'cherry', 'banana', 'apple'] 
+// reverseArray([10, 20, 30]) // [ 30, 20, 10 ] 
