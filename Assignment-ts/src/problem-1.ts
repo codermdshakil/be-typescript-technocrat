fcad68fb991a7278db34f1f30474350a43ea3631
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-29 11:12:31 +0600

    Problem Solved - 1

---
+++ Assignment-ts/src/problem-1.ts
@@ -0,0 +1,34 @@
+
+const repeatString = (stringValue:string, numberValue:number):void => {
+    
+    let result:string = "";
+
+     if(stringValue === ""){
+
+        console.log('Enter Valid String world or any sentence!');
+
+     }else{
+
+        for(let i = 1; i <= numberValue; i++ ){
+
+            result = result + stringValue;
+    
+        }
+    
+        console.log(`"${result}"`);
+
+     }
+   
+
+};
+
+repeatString("Hello!", 3);
+// repeatString("Shakil!", 5);
+
+
+ 
+
+
+
+
+
