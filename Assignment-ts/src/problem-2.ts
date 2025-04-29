4f35078b30a31b6407d1e94910c78c60a6d5c6e7
codermdshakil <ahmedshakil0512@gmail.com>
2025-04-29 11:16:53 +0600

    Problem Solved - 2: find largest number from array

---
+++ Assignment-ts/src/problem-2.ts
@@ -0,0 +1,12 @@
+
+const findLargestNumber = (arr:number[]):void => {
+
+    const res:number = Math.max(...arr);
+    console.log(res);
+
+}
+
+// findLargestNumber([10, 5, 8, 20, 3]); // 20 
+// findLargestNumber([10, 5, 8, 20, 3, 55, 34]); // 55
+
+
